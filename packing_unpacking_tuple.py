1614d19cb5ace2030e85a13bc372078701f4401d
Pravin <Pravin>
2025-04-18 13:47:49 +0530

    Added tuple packing and unpacking code with *

--- packing_unpacking_tuple.py
+++ packing_unpacking_tuple.py
@@ -12,3 +12,12 @@ id,name,phone = packed_tuple
 print(f"id is {id}")
 print(f"name is {name}")
 print(f"phone number is {phone}")
+
+
+## unpacking with *
+numbers = (1,2,3,4,5,6,7,8,9)
+first,*middle,second_last,last = numbers
+print(first)
+print(middle)
+print(second_last)
+print(last)
