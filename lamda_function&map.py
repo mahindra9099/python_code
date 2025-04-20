32b5e552d2c185a82d18506fe3b9a0981bc05d68
Pravin <Pravin>
2025-04-20 21:21:19 +0530

    Lambda function and map

--- lamda_function&map.py
+++ lamda_function&map.py
@@ -8,9 +8,17 @@ print(addition(4,5))
 # map functions takes 2 argument 1 is method and another one is iterable object like list tuple set
 
 in_list= [2,3,4,5,6,7,8,9,10]
-
 print(in_list)
 
-out_list=list(map(lambda x: x**2,in_list))
+# using lamda function
+lambda_list=list(map(lambda x: x**2,in_list))
 
-print(out_list)
+# using normal function
+
+def square(inp):
+    return inp**2
+
+normal_list=list(map(square,in_list))
+
+print(lambda_list)
+print(normal_list)
